f97cb9483cba87f4228763e63fd1232300a0f0bc
LeeEunk <dmsrud7817@naver.com>
2024-11-12 14:04:22 +0900

    update

--- front/util/produce.js
+++ front/util/produce.js
@@ -1,8 +1,8 @@
 // internet explore 낮은 버전에서도 immer 지원하게 함
 
-import produce, {enableES5} from 'immer';
+import {produce } from 'immer';
 
 export default (...args) => {
-    enableES5();
+    
     return produce(...args);
 };
