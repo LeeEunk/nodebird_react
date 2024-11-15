6e578c0fe0dd659f90b79c87049b8b26205b7c3b
LeeEunk <dmsrud7817@naver.com>
2024-11-15 13:53:55 +0900

    port : 3307

--- back/config/config.js
+++ back/config/config.js
@@ -23,6 +23,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": 3307
   }
 }
