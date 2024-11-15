c50cab8d712b8c4c58de0c54f3cd1c864d090cb4
LeeEunk <dmsrud7817@naver.com>
2024-11-15 16:51:09 +0900

    update

--- front/config/config.js
+++ front/config/config.js
@@ -1,1 +1,1 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.35.206.231' : 'http://api.nodebird.com' ;
+export const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.39.254.108' : 'http://api.nodebird.com' ;
