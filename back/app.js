5ee807762ef65f0425febb2478792ef0e638e92b
LeeEunk <dmsrud7817@naver.com>
2024-11-19 11:34:36 +0900

    update

--- back/app.js
+++ back/app.js
@@ -71,8 +71,8 @@ app.use(session({
     secret: process.env.COOKIE_SECRET,
     cookie: {
         httpOnly: true, // 클라이언트에서 쿠키를 사용할 수 없도록 설정
-        secure: true,
-        sameSite: 'None'
+        secure: false,
+        sameSite: 'lax'
         // secure: process.env.NODE_ENV === 'production' ? true : false, // 프로덕션 환경에서만 secure 적용
         // sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 프로덕션에서는 None, 개발 환경에서는 Lax
         // domain: process.env.NODE_ENV === 'production' && 'http://13.209.111.65'
