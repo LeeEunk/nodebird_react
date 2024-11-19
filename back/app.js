c80b451243958022e6a134c3bb57e11bb9dd2459
LeeEunk <dmsrud7817@naver.com>
2024-11-19 14:49:05 +0900

    Fix: cors

--- back/app.js
+++ back/app.js
@@ -32,29 +32,28 @@ db.sequelize.sync()
 passportConfig();
 
 // 프론트에서 백엔드에 어떤 요청이 있는지 보여줌
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(morgan('combined'));
-//     app.use(hpp());
-//     app.use(helmet({ contentSecurityPolicy: false}));
-//     app.options('*', cors());
-//     app.use(cors({
-//         origin: 'http://3.35.220.149',
-//         credentials: true,
-//     }));
-// } else {
-//     app.use(morgan('dev'));
-//     app.use(cors({
-//         origin: true,
-//         credentials: true,
-//     }));
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(hpp());
+    app.use(helmet());
+    app.use(cors({
+        origin: 'http://eunkk.store',
+        credentials: true,
+    }));
+} else {
+    app.use(morgan('dev'));
+    app.use(cors({
+        origin: true,
+        credentials: true,
+    }));
+}
 
 // router보다 늘 위에 있어야 함 위에서 아래로 실행되므로
 // req.body 인식
-app.use(cors({
-    origin: ['http://localhost:3060', 'http://nodebird.com'], // * 대신 보낸 곳의 주소가 자동으로 들어가야 보안이 높아짐, 단, true로 해도 무방.
-    credentials: true, // 기본값은 false임, 이게 트루면 origin을 정확한 주소로 표기해야 함, 쿠키를 전송하기 위해 설정
-}));
+// app.use(cors({
+//     origin: ['http://localhost:3060', 'http://eunkk.store'], // * 대신 보낸 곳의 주소가 자동으로 들어가야 보안이 높아짐, 단, true로 해도 무방.
+//     credentials: true, // 기본값은 false임, 이게 트루면 origin을 정확한 주소로 표기해야 함, 쿠키를 전송하기 위해 설정
+// }));
 
 app.use('/', express.static(path.join(__dirname, 'uploads'))) // image 백엔드 서버로 보낼때, os 차이때문에 경로의 문제가 될 수 있으므로 join을 함
 app.use(express.json()) //axios로 데이터 보낼때
