a9e7d478383d0d615f4d0a7fd5f1548313edd0b2
LeeEunk <dmsrud7817@naver.com>
2024-11-20 17:05:51 +0900

    rollback

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -228,7 +228,7 @@ function* addPost(action) {
 }
 
 function updatePostAPI(data) {
-    return axios.patch(`/post/${data.postId}`, data); 
+    return axios.patch(`/post/${data.PostId}`, data); 
 }
 
 function* updatePost(action) {
