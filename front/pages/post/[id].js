4ed8fcb9d7b28c3e8a4358527d04608e5ab62460
LeeEunk <dmsrud7817@naver.com>
2024-11-25 11:08:54 +0900

    update

--- front/pages/post/[id].js
+++ front/pages/post/[id].js
@@ -7,7 +7,7 @@ import { useSelector } from 'react-redux';
 import Head from 'next/head';
 
 import wrapper from '../../store/configureStore';
-import { LOAD_MY_INFO_REQUEST, LOAD_USER_REQUEST } from '../../reducers/user';
+import { LOAD_MY_INFO_REQUEST } from '../../reducers/user';
 import { LOAD_POST_REQUEST } from '../../reducers/post';
 import AppLayout from '../../components/AppLayout';
 import PostCard from '../../components/PostCard';
@@ -50,26 +50,21 @@ const Post = () => {
 //   };
 // }
 
-export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-  const cookie = context.req ? context.req.headers.cookie : '';
-  console.log(context);
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req, params }) => {
+  const cookie = req ? req.headers.cookie : '';
   axios.defaults.headers.Cookie = '';
-  if (context.req && cookie) {
+  if (req && cookie) {
     axios.defaults.headers.Cookie = cookie;
   }
-  context.store.dispatch({
+  store.dispatch({
     type: LOAD_MY_INFO_REQUEST,
   });
-  context.store.dispatch({
-    type: LOAD_USER_REQUEST,
-    data: context.params.id,
-  });
-  context.store.dispatch({
+  store.dispatch({
     type: LOAD_POST_REQUEST,
-    data: context.params.id,
+    data: params.id,
   });
-  context.store.dispatch(END);
-  await context.store.sagaTask.toPromise();
+  store.dispatch(END);
+  await store.sagaTask.toPromise();
 });
 
 export default Post;
