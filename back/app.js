c05b1020e0e3b04868914007a0b52f7099455997
LeeEunk <dmsrud7817@naver.com>
2024-11-26 15:12:44 +0900

    cookie 안됨 ㅜ

--- back/app.js
+++ back/app.js
@@ -1,12 +1,9 @@
-// 중앙 관리자
-// app.js에 들어가는 것들은 모두 미들웨어임 -> req, res, next 이런 모양임
-// node가 서버가 아니라 node에서 제공하는 http 모듈이 서버임 
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
 const hpp = require('hpp');
@@ -20,34 +17,32 @@ const db = require('./models');
 const passportConfig = require('./passport');
 
 dotenv.config();
-
 const app = express();
-
 // 실행전에 npx sequelize db:create 날려줘야함
 db.sequelize.sync()
-    .then(() => {
-        console.log('db 연결 성공');
-    })
-    .catch(console.error);
+  .then(() => {
+    console.log('db 연결 성공');
+  })
+  .catch(console.error);
 passportConfig();
-
 // 프론트에서 백엔드에 어떤 요청이 있는지 보여줌
 if (process.env.NODE_ENV === 'production') {
-    app.enable('trust proxy');
-    app.use(morgan('combined'));
-    app.use(hpp());
-    app.use(helmet( {contentSecurityPolicy: false}));
-    app.use(cors({
-        origin: 'https://eunkk.store',
-        credentials: true,
-    }));
+  app.enable('trust proxy');
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(cors({
+    origin: 'https://eunkk.store',
+    credentials: true,
+  }));
 } else {
-    app.use(morgan('dev'));
-    app.use(cors({
-        origin: true,
-        credentials: true,
-    }));
+  app.use(morgan('dev'));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
 }
+app.use('/', express.static(path.join(__dirname, 'uploads')));
 
 app.use((err, req, res, next) => {
     if (err.code === 'LIMIT_FILE_SIZE') {
@@ -62,9 +57,7 @@ app.use((err, req, res, next) => {
 //     origin: ['http://localhost:3060', 'http://eunkk.store'], // * 대신 보낸 곳의 주소가 자동으로 들어가야 보안이 높아짐, 단, true로 해도 무방.
 //     credentials: true, // 기본값은 false임, 이게 트루면 origin을 정확한 주소로 표기해야 함, 쿠키를 전송하기 위해 설정
 // }));
-
-app.use('/', express.static(path.join(__dirname, 'uploads'))) // image 백엔드 서버로 보낼때, os 차이때문에 경로의 문제가 될 수 있으므로 join을 함
-app.use(express.json()) //axios로 데이터 보낼때
+app.use(express.json()); //axios로 데이터 보낼때
 app.use(express.urlencoded({ extended: true })); //multi-part가 아닌 일반폼으로 데이터 보낼때
 // cookie 설정 -> random한 토큰값 전달 (개인정보 해킹을 막기 위함) -> 브라우저는 누구나 값을 읽을 수 있으므로 해킹에 취약하므로
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -73,44 +66,29 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // session 설정 -> 쿠키값과 세션값을 연결하기 위해 토큰값을 들고 있음
 app.use(session({
-    saveUninitialized: false,
-    resave: false,
-    secret: process.env.COOKIE_SECRET,
-    proxy: process.env.NODE_ENV === 'production',
-    cookie: {
-        httpOnly: true, // 클라이언트에서 쿠키를 사용할 수 없도록 설정
-        secure: true,
-        // secure: process.env.NODE_ENV === 'production' ? true : false, // 프로덕션 환경에서만 secure 적용
-        // sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // 프로덕션에서는 None, 개발 환경에서는 Lax
-        domain: process.env.NODE_ENV === 'production' && '.eunkk.store'
-    },
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.COOKIE_SECRET,
+  proxy: process.env.NODE_ENV === 'production',
+  cookie: {
+    httpOnly: true, // 클라이언트에서 쿠키를 사용할 수 없도록 설정
+    secure: process.env.NODE_ENV === 'production',
+    domain: process.env.NODE_ENV === 'production' && '.eunkk.store'
+  },
 }));
-
-
 // passport 초기화 및 세션 설정
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.get('/', (req, res) => {
-    res.send('hello express')
+  res.send('hello express');
 });
 
-app.get('/api', (req, res) => {
-    res.send('hello api')
-});
-
-// 라우터 순서가 매우 중요
 app.use('/posts', postsRouter);
 app.use('/post', postRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-// // default로 에러처리 미들웨어가 장착되어있음 커스터마이징하려면 여기를 수정하면 됨
-// app.use((err, req, res, next) => {
-
-// });
-
 app.listen(3065, () => {
-    console.log("서버 실행 중.... http://localhost:3065");
+  console.log('서버 실행 중.....');
 });
