2483a1a327d0f8c3d4044c189a2738cb574c2f3a
LeeEunk <dmsrud7817@naver.com>
2024-11-27 15:33:26 +0900

    index 추가

--- back/models/index.js
+++ back/models/index.js
@@ -4,6 +4,7 @@ const hashtag = require('./hashtag');
 const image = require('./image');
 const post = require('./post');
 const user = require('./user');
+const report = require('./report');
 
 const env = process.env.NODE_ENV || 'development'; // 기본값 development
 const config = require('../config/config')[env];
@@ -17,6 +18,7 @@ db.Hashtag = hashtag;
 db.Image = image;
 db.Post = post;
 db.User = user;
+db.Report = report;
 
 Object.keys(db).forEach(modelName => {
   db[modelName].init(sequelize);
